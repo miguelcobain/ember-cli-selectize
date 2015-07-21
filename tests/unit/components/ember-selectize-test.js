897c281393cfde977dfe611e136f3df90e3615e6
Miguel Andrade <jmandrade0@gmail.com>
2015-07-21 12:00:25 +0100

    run in unit mode explicitly

--- tests/unit/components/ember-selectize-test.js
+++ tests/unit/components/ember-selectize-test.js
@@ -7,6 +7,7 @@ import {
 moduleForComponent('ember-selectize', 'Unit | Component | ember-selectize', {
   // Specify the other units that are required for this test
   // needs: ['component:foo', 'helper:bar']
+  unit: true
 });
 
 test('it renders', function(assert) {
