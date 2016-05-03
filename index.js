4d656388aab914c2134cb378919d9ebf70027363
Mike North <michael.l.north@gmail.com>
2016-05-03 23:43:58 +0800

    Fastboot compatibility

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ module.exports = {
     //default theme name is 'default'
     var options = app.options['ember-cli-selectize'] || { theme: 'default' };
 
-    //import theme based on options
-    if (options.theme){
-      app.import(app.bowerDirectory + '/selectize/dist/css/selectize.' + options.theme + '.css');
-    }
+    if (process.env.EMBER_CLI_FASTBOOT !== 'true') {
+      //import theme based on options
+      if (options.theme){
+        app.import(app.bowerDirectory + '/selectize/dist/css/selectize.' + options.theme + '.css');
+      }
 
-    //import javascript
-    app.import(app.bowerDirectory + '/selectize/dist/js/standalone/selectize.js');
+      //import javascript
+      app.import(app.bowerDirectory + '/selectize/dist/js/standalone/selectize.js');
+    }
   }
 };
